234ec35cd89cd9c9e3c313f948d55bced4f2f46a
pingplass123 <60729252+pingplass123@users.noreply.github.com>
2022-03-23 20:42:25 +0700

    init

---
+++ app/Http/Controllers/API/PostController.php
@@ -0,0 +1,114 @@
+<?php
+   
+namespace App\Http\Controllers\API;
+   
+use Illuminate\Http\Request;
+use App\Http\Controllers\API\BaseController as BaseController;
+use App\Http\Resources\PostResource;
+use App\Models\Post;
+use Validator;
+use Auth;
+   
+class PostController extends BaseController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::all();
+    
+        return $this->sendResponse(PostResource::collection($posts), 'Post retrieved successfully.');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $input = $request->all();
+   
+        $validator = Validator::make($input, [
+            'title' => 'required',
+            'body' => 'required',
+            
+        ]);
+   
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());       
+        }
+        
+        $post = Post::create([
+            'body' => request('body'),
+            'title' => request('title'),
+            'userID' => Auth::id()
+        ]);
+    
+        
+        return $this->sendResponse(new PostResource($post), 'Post created successfully.');
+    } 
+   
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $post = Post::find($id);
+  
+        if (is_null($post)) {
+            return $this->sendError('Post not found.');
+        }
+   
+        return $this->sendResponse(new PostResource($post), 'Post retrieved successfully.');
+    }
+    
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        $input = $request->all();
+
+        $validator = Validator::make($input, [
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());
+        }
+
+        
+        $post->title = $input['title'];
+        $post->body = $input['body'];
+        $post->userID = Auth::id();
+        $post->save();
+
+        return $this->sendResponse(new PostResource($post), 'Post updated successfully.');
+    }
+   
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        $post->delete();
+   
+        return $this->sendResponse([], 'Post deleted successfully.');
+    }
+}
